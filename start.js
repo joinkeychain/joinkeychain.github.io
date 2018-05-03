3cd4f1dd951e7b23cdf2253d229c9c16351e2f08
joinkeychain <38946755+joinkeychain@users.noreply.github.com>
2018-05-03 16:51:41 +0200

    Update start.js

--- start.js
+++ start.js
@@ -1,22 +1,22 @@
 
-// jQuery.noConflict();
-// (function($) {
-//   $(function() {
+jQuery.noConflict();
+(function($) {
+  $(function() {
+    var data = { type: "FROM_URL", text: "Hello from the webpage!" };
+    window.postMessage(data, "*");
 
-//   });
-// })(jQuery);
+    var config = {
+      apiKey: "AIzaSyDsYOfdNSuPPkJeoNlOCz9c4TUX-ln3Op0",
+      authDomain: "keychain-web.firebaseapp.com",
+      databaseURL: "https://keychain-web.firebaseio.com",
+      projectId: "keychain-web",
+      storageBucket: "keychain-web.appspot.com",
+      messagingSenderId: "269609860531"
+    };
+    firebase.initializeApp(config);
 
-var data = { type: "FROM_URL", text: "Hello from the webpage!" };
-window.postMessage(data, "*");
+    var db = firebase.database();
+  });
+})(jQuery);
 
-var config = {
-  apiKey: "AIzaSyDsYOfdNSuPPkJeoNlOCz9c4TUX-ln3Op0",
-  authDomain: "keychain-web.firebaseapp.com",
-  databaseURL: "https://keychain-web.firebaseio.com",
-  projectId: "keychain-web",
-  storageBucket: "keychain-web.appspot.com",
-  messagingSenderId: "269609860531"
-};
-firebase.initializeApp(config);
 
-var db = firebase.database();
