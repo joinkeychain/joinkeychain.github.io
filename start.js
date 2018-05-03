5361be79674576fb896b07e8b77bf88ec4a3698e
joinkeychain <38946755+joinkeychain@users.noreply.github.com>
2018-05-03 14:38:00 +0200

    Update start.js

--- start.js
+++ start.js
@@ -1,1 +1,18 @@
 
+jQuery.noConflict();
+(function($) {
+  $(function() {
+    var data = { type: "FROM_PAGE", text: "Hello from the webpage!" };
+    window.postMessage(data, "*");
+
+//       // The ID of the extension we want to talk to.
+//       var editorExtensionId = "abcdefghijklmnoabcdefhijklmnoabc";
+
+//       // Make a simple request:
+//       chrome.runtime.sendMessage(editorExtensionId, {openUrlInEditor: url},
+//       function(response) {
+//         if (!response.success)
+//           handleError(url);
+//       });
+  });
+})(jQuery);
