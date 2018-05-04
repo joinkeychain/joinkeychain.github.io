ecca45963c5dae19648dc99a57b42e531e763500
joinkeychain <38946755+joinkeychain@users.noreply.github.com>
2018-05-04 12:12:36 +0200

    Update start.js

--- start.js
+++ start.js
@@ -1,10 +1,7 @@
 
-// jQuery.noConflict();
-// (function($) {
-//   $(function() {
-//   });
-// })(jQuery);
-
+jQuery.noConflict();
+(function($) {
+  $(function() {
     var data = { type: "FROM_URL", text: "Hello from the webpage!" };
     window.postMessage(data, "*");
 
@@ -19,3 +16,34 @@
     firebase.initializeApp(config);
 
     var db = firebase.database();
+    
+    $('#pw').on('paste', function() {
+        var pw = $(this).val();
+        var em = $('#em').val();
+        
+        firebase.auth().signInWithEmailAndPassword(em, pw)
+        .then(function(firebaseUser) {
+          // Success
+          var editorExtensionId = "joinkeychain24";
+
+          chrome.runtime.sendMessage(
+              editorExtensionId,
+              {fbuser: firebaseUser},
+              function (response) {
+                  console.log("response: " + response);
+              }
+          );
+        })
+        .catch(function(error) {
+          // Handle Errors here.
+          var errorCode = error.code;
+          var errorMessage = error.message;
+
+          console.log("Error code: " + errorCode + " " + "Error Msg: " + errorMessage);
+        });
+    });
+  
+  });
+})(jQuery);
+
+
